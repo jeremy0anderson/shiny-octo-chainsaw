bcad93873c4a9d6c0d6b57e26fb4f730355b1e16
Rachel <rachelmmarriott@gmail.com>
2022-03-23 20:01:42 -0600

    initial files and database connection

---
+++ controllers/apiRoutes/host-routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { Host, Player, King } = require('../../models');
+
+router.get('/', (req, res) => {
+  
+})
+
+router.post('/', (req, res) => {
+
+})
+
+router.put('/:id', (req, res) => {
+
+})
+
+router.delete('/:id', (req, res) => {
+  
+})
+
+module.exports = router;
