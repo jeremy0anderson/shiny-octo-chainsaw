bc50d17e94c00275cd35b01661ab5dd4ba3b8c7a
ilantimerman <ilan.tmrmn@gmail.com>
2022-03-26 12:43:54 -0700

    finished up login page

--- app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = require('express').Router();
-const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes')
-
-app.use('/api', apiRoutes);
