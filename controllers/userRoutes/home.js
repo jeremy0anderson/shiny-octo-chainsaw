994980a7206aebc52463d315f33b0ae505c88b92
lillyleiran <lilly.leiran@gmail.com>
2022-03-29 13:27:10 -0600

    update home.js trying to make partials work, they mostly don't :(

--- controllers/userRoutes/home.js
+++ controllers/userRoutes/home.js
@@ -1,5 +1,11 @@
+const handlebars = require('express-handlebars');
 const router = require('express').Router();
 
+const {Role} = require('../../Role');
+
+const homepage = handlebars.registerPartial('homepage', function() {  })
+const king = handlebars.registerPartial('king', function() {  })
+const player = handlebars.registerPartial('player', function() {  })
 
 function redirectUser(req, res, next){
     if (req.session.user){
@@ -7,6 +13,41 @@ function redirectUser(req, res, next){
     }
     next();
 }
+
+const kingHomeRender={
+    king:{
+        role:[{
+            name: ,
+            loyalty: ,
+            ability: ,
+            winCondition1: ,
+            winCondition2: ,
+            footnote:
+        }],
+        method:{
+            type: "king",
+            opposite: "player",
+            oppositeRoute: "/home/king"
+        }
+    }
+},
+playerHomeRender={
+    player:{
+        role:[{
+            name: ,
+            loyalty: ,
+            ability: ,
+            winCondition1: ,
+            winCondition2: ,
+            footnote:
+        }],
+        method:{
+            type: "player",
+            opposite: "king",
+            oppositeRoute: "/home/player"
+        }
+    }
+};
 
 router.get('/',redirectUser,(req, res)=>{
     res.render('home');
