5a020472903c43d4f2863164f588e76ae2046699
Rachel Marriott <69021829+Rachel8078@users.noreply.github.com>
2022-03-30 10:30:29 -0600

    working on handlebars

--- models/index.js
+++ models/index.js
@@ -2,32 +2,32 @@ const Host = require('./Host');
 const Game = require('./Game');
 const Player = require('./Player');
 const Role = require('./Role');
-const King = require('./Room');
+const Room = require('./Room');
 
 
-// create associations
-Host.belongsTo(Game, {
-  foreignKey: 'game_id'
-});
+// // create associations
+// Host.belongsTo(Game, {
+//   foreignKey: 'game_id'
+// });
 
-Host.hasMany(Player, {
-  foreignKey: 'host_id'
-});
+// Host.hasMany(Player, {
+//   foreignKey: 'host_id'
+// });
 
-Player.hasOne(Host, {
-  foreignKey: 'host_id'
-});
+// Player.hasOne(Host, {
+//   foreignKey: 'host_id'
+// });
 
-Player.belongsTo(Game, {
-  foreignKey: 'game_id'
-})
+// Player.belongsTo(Game, {
+//   foreignKey: 'game_id'
+// })
 
-Player.hasOne(Role, {
-  foreignKey: 'role_id'
-});
+// Player.hasOne(Role, {
+//   foreignKey: 'role_id'
+// });
 
-Room.hasMany(Player, {
-  foreignKey: 'room_id'
-})
+// Room.hasMany(Player, {
+//   foreignKey: 'room_id'
+// })
 
 module.exports = { Player, Host, Role, Game, Room };
