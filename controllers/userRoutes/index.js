290804b496851ba0df723a589028418cc55831e8
lillyleiran <lilly.leiran@gmail.com>
2022-03-30 22:58:34 -0600

    update all available pages to work through the main.handlebars file with paths pulling them up

--- controllers/userRoutes/index.js
+++ controllers/userRoutes/index.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 const {configureSession, sequelize} = require('../../config/connection');
 const dashboardRoute = require('./dashboard');
 const authRoute = require('./auth');
-const hostRoute = require('./game');
+const gameRoute = require('./game');
 const homeRoute = require('./home');
 const lobbyRoute = require('./lobby');
 
 configureSession(router);
 
 router.use('/dashboard', dashboardRoute);
-router.use('/game', hostRoute);
+router.use('/game', gameRoute);
 router.use('/', authRoute);
 router.use('/home', homeRoute);
 router.use('/lobby', lobbyRoute);
